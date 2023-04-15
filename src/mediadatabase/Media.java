8b4f7626bd51834bbc3e5147c3e9ee7a8bc40d70
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-04-15 14:48:53 +0300

    Implemented Media.java

--- src/mediadatabase/Media.java
+++ src/mediadatabase/Media.java
@@ -1,3 +1,54 @@
 abstract class Media {
+    private String type; // Could be "movie" or "music"
+    private String title; // name of the media
+    private String location; // filename or URL of the media
+    private int year; // year it was released
+    private String category; // genre
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public abstract void playMedia();
+
+    public String toString() {
+        return "Media{type = " + type + ", title = " + title + ", location = " + location + ", year = " + year
+                + ", category = " + category + "}";
+    }
 }
