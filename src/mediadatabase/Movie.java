f0ac37e83291fea9fdc85fc34ebc140125739dee
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-04-15 14:37:40 +0300

    Initial commit; includes errors

---
+++ src/mediadatabase/Movie.java
@@ -0,0 +1,3 @@
+class Movie extends Media {
+
+}
---
+++ src/mediadatabase/MyMediaDatabase.java
@@ -0,0 +1,27 @@
+class MyMediaDatabase implements MediaDatabase {
+
+    @Override
+    public Media lookupByTitle(String title) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lookupByTitle'");
+    }
+
+    @Override
+    public void addEntry(Media newMedia) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addEntry'");
+    }
+
+    @Override
+    public void listAllMedia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listAllMedia'");
+    }
+
+    @Override
+    public void listMediaByType(String type) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listMediaByType'");
+    }
+
+}
