ee2cc489c90806aa012caa82d9b5e1555e5004fc
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-04-19 16:33:16 +0300

    Finished Music class implementation

--- src/mediadatabase/Music.java
+++ src/mediadatabase/Music.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Music extends Media {
     private String artist;
     private int numberOfSongs;
@@ -5,6 +7,45 @@ class Music extends Media {
     public Music(String title, String location, int year, String category, String artist, int numberOfSongs) {
         super("Music", title, location, year, category);
         this.artist = artist;
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    @Override
+    public void playMedia() {
+        Scanner scIn = new Scanner(System.in);
+        int choice;
+        if (numberOfSongs > 1) {
+            System.out.println("Which track do you want to play (Track 1 - " + numberOfSongs + ")?");
+            choice = scIn.nextInt();
+        } else {
+            System.out.println("Do you want to play Track 1 (y/n)?");
+            char playChoice = scIn.next().toLowerCase().charAt(0);
+            if (playChoice == 'y') {
+                choice = 1;
+            } else {
+                // else don't play Track
+                scIn.close();
+                return;
+            }
+        }
+
+        scIn.close();
+        System.out.println("Now playing: Track " + choice + " of " + getTitle());
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
         this.numberOfSongs = numberOfSongs;
     }
 
