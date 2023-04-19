fd86e494b5271a50a4d2db53121215c6b67e5d74
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-04-19 18:21:30 +0300

    Implemented MyMediaDatabase

--- src/mediadatabase/MyMediaDatabase.java
+++ src/mediadatabase/MyMediaDatabase.java
@@ -1,27 +1,59 @@
+import java.util.ArrayList;
+
 class MyMediaDatabase implements MediaDatabase {
+
+    private ArrayList<Media> mediaList;
+
+    public MyMediaDatabase() {
+        mediaList = new ArrayList<>();
+    }
 
     @Override
     public Media lookupByTitle(String title) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lookupByTitle'");
+        for (int i = 0; i < mediaList.size(); i++) {
+            Media currMedia = mediaList.get(i);
+            if (currMedia.getTitle().equals(title)) {
+                return currMedia;
+            }
+        }
+
+        return null;
     }
 
     @Override
     public void addEntry(Media newMedia) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addEntry'");
+        boolean exists = false;
+        for (int i = 0; i < mediaList.size(); i++) {
+            Media currMedia = mediaList.get(i);
+            if (currMedia.getTitle().equals(newMedia.getTitle())) {
+                exists = true;
+            }
+        }
+
+        if (!exists) {
+            mediaList.add(newMedia);
+            System.out.println("Media added successfully");
+        } else {
+            System.out.println("Media with that title " + newMedia.getTitle() + " already exists!");
+        }
     }
 
     @Override
     public void listAllMedia() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listAllMedia'");
+        for (int i = 0; i < mediaList.size(); i++) {
+            Media currMedia = mediaList.get(i);
+            System.out.println(currMedia.getType() + ": " + currMedia.getTitle());
+        }
     }
 
     @Override
     public void listMediaByType(String type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listMediaByType'");
+        for (int i = 0; i < mediaList.size(); i++) {
+            Media currMedia = mediaList.get(i);
+            if (currMedia.getType().equals(type)) {
+                System.out.println(currMedia);
+            }
+        }
     }
 
 }
