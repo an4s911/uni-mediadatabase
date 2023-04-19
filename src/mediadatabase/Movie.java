393b0194bd72dead97207eb0b801ecd1f455dc93
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-04-19 16:33:04 +0300

    Finished Movie class implementation

--- src/mediadatabase/Movie.java
+++ src/mediadatabase/Movie.java
@@ -5,6 +5,28 @@ class Movie extends Media {
     public Movie(String title, String location, int year, String category, String director, int playingTime) {
         super("Movie", title, location, year, category);
         this.director = director;
+
+        this.playingTime = playingTime;
+    }
+
+    @Override
+    public void playMedia() {
+        System.out.println("Now playing: " + getTitle());
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getPlayingTime() {
+        return playingTime;
+    }
+
+    public void setPlayingTime(int playingTime) {
         this.playingTime = playingTime;
     }
 
